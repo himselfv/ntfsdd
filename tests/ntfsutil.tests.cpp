1c509a686129bd65f443ccb6b994c5c3ca7d53da
himselfv@gmail.com <himselfv@gmail.com>
2026-02-27 19:12:01 +0300

    Some tests for SlicedRunIterator.

---
+++ tests/ntfsutil.tests.cpp
@@ -0,0 +1,43 @@
+#include "catch.hpp"
+#include "bitmap.h"
+#include "ntfsutil.h"
+
+int rand_int(int min, int max)
+{
+	double pt = (double)rand() / RAND_MAX;
+	return min + (pt) * max;
+}
+
+void randomFill(Bitmap& bmp)
+{
+	for (size_t i = 0; i < 100; i++) {
+		int len = rand_int(0, bmp.size / 100);
+		int start = rand_int(0, bmp.size - len - 1);
+		bmp.set(ClusterRun{ start, len });
+	}
+}
+
+TEST_CASE("SlicedRunEquivalence", "[ClusterRun]") {
+	BitmapBuf bmp;
+	bmp.resize(4096);
+
+	BitmapBuf result1;
+	BitmapBuf result2;
+	result1.resize(bmp.size);
+	result2.resize(bmp.size);
+
+	for (size_t i = 0; i < 100; i++) {
+		randomFill(bmp);
+		result1.clear_all();
+		result2.clear_all();
+		int slice_size = i+1; //Just try all various slice sizes
+
+		for (auto& span : BitmapSpans(&bmp))
+			result1.set(span);
+
+		for (auto& span : slice_runs(BitmapSpans(&bmp), slice_size))
+			result2.set(span);
+
+		CHECK(0 == Bitmap::memcmp(result1.data, result2.data, result1.size));
+	}
+}
